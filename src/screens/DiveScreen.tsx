e0fb997dd8e3ba7ab9293a60a1d7e19118b06def
Brice Boudehent <brice.boudehent@gmail.com>
2023-07-21 21:42:13 +0200

    adding inputs

--- src/screens/DiveScreen.tsx
+++ src/screens/DiveScreen.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 const DiveScreen = () => {
+  const [depth, setDepth] = useState('');
+  const [duration, setDuration] = useState('');
+  const [tankMaterial, setTankMaterial] = useState('steel');
+
+  const handleSubmit = () => {
+    console.log('--------- Form submitted -----------');
+    console.log(depth, duration, tankMaterial);
+  };
+
   return (
     <View>
       <Text>Page d'ajout de plongées</Text>
+      <TextInput
+        placeholder="Depth"
+        value={depth}
+        onChangeText={text => setDepth(text)}
+        keyboardType="numeric"
+      />
+      <TextInput
+        placeholder="Duration"
+        value={duration}
+        onChangeText={text => setDuration(text)}
+        keyboardType="numeric"
+      />
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            setTankMaterial('aluminum');
+          }}>
+          <Text>Aluminum</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setTankMaterial('steel');
+          }}>
+          <Text>Steel</Text>
+        </TouchableOpacity>
+      </View>
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
