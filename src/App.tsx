d7d84be6259b6fb0aaca741d8ad30c6ffdb7eb1d
Brice Boudehent <brice.boudehent@gmail.com>
2023-07-29 23:45:32 +0200

    design improvements

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import {StyleSheet, View} from 'react-native';
 import AppNavigation from './navigation/AppNavigation';
 
 const styles = StyleSheet.create({
-  container: {height: '100%', backgroundColor: 'white'},
-  menu: {
-    position: 'absolute',
-    bottom: 0,
-  },
+  container: {display: 'flex', flex: 1},
 });
 
 export default function App() {
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import Account from '../assets/account.svg';
+import Home from '../assets/home.svg';
 import Map from '../assets/map.svg';
+import ScubaDiving from '../assets/scuba_diving.svg';
 import DiveScreen from '../screens/DiveScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SiteScreen from '../screens/SiteScreen';
@@ -23,31 +26,56 @@ const AppNavigation = () => {
   };
 
   const styles = StyleSheet.create({
-    container: {
+    appContainer: {display: 'flex', flex: 1},
+    pagesContainer: {display: 'flex', flex: 1},
+    menuContainer: {
+      position: 'absolute',
+      bottom: 10,
+      left: 0,
+      right: 0,
       display: 'flex',
       justifyContent: 'space-around',
       alignItems: 'center',
       flexDirection: 'row',
       borderRadius: 10,
       backgroundColor: globalStyles.primaryColor,
-      marginBottom: 10,
-      marginHorizontal: 15,
-      paddingVertical: 10,
+      marginHorizontal: 10,
+      paddingVertical: 7,
     },
-    button: {width: 40, height: 40},
-    icon: {width: 30, height: 20},
+    button: {
+      height: 40,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    icon: {width: 23},
   });
 
   return (
-    <View>
-      <View style={styles.container}>
-        <TouchableOpacity onPress={() => setScreen('Dives')}>
+    <View style={styles.appContainer}>
+      <View style={styles.pagesContainer}>{renderScreen()}</View>
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setScreen('Dives')}>
+          <Home width={styles.icon.width} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setScreen('Dives')}>
+          <ScubaDiving width={styles.icon.width} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setScreen('Sites')}>
           <Map width={styles.icon.width} />
         </TouchableOpacity>
-        <Button title="Sites" onPress={() => setScreen('Sites')} />
-        <Button title="Profile" onPress={() => setScreen('Profile')} />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setScreen('Profile')}>
+          <Account width={styles.icon.width} />
+        </TouchableOpacity>
       </View>
-      {renderScreen()}
     </View>
   );
 };
